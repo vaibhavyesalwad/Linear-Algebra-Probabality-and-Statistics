4313bde519b2b17b11eb955c790496113775bdc1
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-05 16:59:37 +0530

    Added: Comment- describing condition for addition of matrices

--- Linear Algebra/01_AddMatrices.py
+++ Linear Algebra/01_AddMatrices.py
@@ -1,7 +1,7 @@
 """Program to add matrices"""
 X = [[12, 7, 3],
-     [4, 5, 6],
-     [7, 8, 9]]
+     [4, 5, 6],               # For addition of matrices order of matrices must be same
+     [7, 8, 9]]               # Both matrices X and Y are of order 3*3
 Y = [[5, 8, 1],
      [6, 7, 3],
      [4, 5, 9]]
