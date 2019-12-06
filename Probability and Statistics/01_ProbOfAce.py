149de49e2db2c23fa030173103419a99c459b893
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-06 18:33:28 +0530

    Added: ProbOfAce- finding probability of drawing an ace from pack of cards

---
+++ Probability and Statistics/01_ProbOfAce.py
@@ -0,0 +1,6 @@
+"""Program to find probability of drawing an ace from pack of cards"""
+from fractions import Fraction as F           # using fractions module
+cards = 52        # total cards in total pack
+aces = 4           # total ace cards
+print(f'Probability of an ace from pack of cards: {F(aces, cards)}')       # representing probability in fraction
+# print(f'Probability if of an ace: {round(aces/cards,2)}') # representing probability in floats
