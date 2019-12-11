4f25436941c3ec10353941b155cad21523300957
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-11 17:38:35 +0530

    Refactored: changed variable names

--- Linear Algebra/06_TransposeOfMatrix.py
+++ Linear Algebra/06_TransposeOfMatrix.py
@@ -1,9 +1,9 @@
 """Program to find transpose matrix of matrix given 3*3 matrix"""
-Y = [[5, 8, 1],
-     [6, 7, 3],           # given 3*3 matrix
-     [4, 5, 9]]
+mat1 = [[5, 8, 1],
+        [6, 7, 3],           # given 3*3 matrix
+        [4, 5, 9]]
 
 # Transpose of matrix is changing rows to columns and vice versa
-transpose = [[Y[j][i] for j in range(3)] for i in range(3)]
+transpose = [[mat1[col][row] for col in range(3)] for row in range(3)]
 for row in transpose:
     print(row)             # displaying result
