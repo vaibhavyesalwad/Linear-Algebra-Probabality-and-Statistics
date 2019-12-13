294598fb8aa0593433c8f746479d74d9c9906ef3
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-13 15:40:55 +0530

    Refactored: replaced hardcoded values by input values and done exception handling

--- Linear Algebra/03_VectorMultiMatrix.py
+++ Linear Algebra/03_VectorMultiMatrix.py
@@ -1,12 +1,48 @@
 """Perform multiplication of given matrix and vector"""
-X = [[5, 1, 3],
-     [1, 1, 1],      # vector multiplication of matrix possible only when no. of columns in matrix equals rows in vector
-     [1, 2, 1]]         # matrix is order 3*3
-Y = [1, 2, 3]       # vector considered as single column 3*1
 
-# resultant matrix will of size 3*1((columns in matrix)*1)
+# taking order of matrix from user
+while True:
+    try:
+        rows = int(input('Enter number of rows for X matrix:'))
+        cols = int(input('Enter number of columns for X matrix:'))
+        while True:
+            try:
+                # vector is single column/row matrix eventually considered column matrix
+                vector_rows = int(input('Enter number of rows/columns in vector matrix:'))
+                assert vector_rows == cols             # condition for vector multiplication
+                break
+            except ValueError:
+                print('Enter only integer for number of rows in vector')
+            except AssertionError:
+                print("Number of columns in matrix & number of rows in vector don't match")
+        break
+    except ValueError:
+        print('Enter only integers')
+
+print("Let's start filling matrix X")
+# taking values for matrix from user
+while True:
+    try:
+        X = [[int(input(f'Enter value for X[{row}][{col}]:')) for col in range(cols)] for row in range(rows)]
+        input("Now filling vector")
+        vector = [int(input(f'Enter value for vector[{row}]:')) for row in range(vector_rows)]
+        break
+    except ValueError:
+        print('Please enter only integer values')
+
+# displaying matrix
+print('Matrix X')
+for row in X:
+    print(row)
+
+# displaying vector
+print('Vector')
+for num in vector:
+    print(f'[{num}]')
+
 # multiplying columns of matrix with single column of vector
-vector_multi = [[X[i][j]*Y[j] for j in range(3)] for i in range(3)]
+vector_multi = [sum(X[row][col]*vector[col] for col in range(cols)) for row in range(rows)]
 
+print('Vector multiplication of matrix X & above vector')
 for row in vector_multi:
-    print(row)               # displaying result
+    print(f'[{row}]')               # displaying result
