ffb495e00c768f05c96dbea489c2352c1cae9eaa
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-13 19:08:37 +0530

    Refactored: replaced hardcoded values by user inputs

--- Linear Algebra/06_TransposeOfMatrix.py
+++ Linear Algebra/06_TransposeOfMatrix.py
@@ -1,9 +1,31 @@
 """Program to find transpose matrix of matrix given 3*3 matrix"""
-mat1 = [[5, 8, 1],
-        [6, 7, 3],           # given 3*3 matrix
-        [4, 5, 9]]
+# taking order of matrix from user
+while True:
+    try:
+        rows = int(input('Enter number of rows for matrix:'))
+        cols = int(input('Enter number of columns for matrix:'))
+        break
+    except ValueError:             # making sure integer input values
+        print('Enter only integers')
 
-# Transpose of matrix is changing rows to columns and vice versa
-transpose = [[mat1[col][row] for col in range(3)] for row in range(3)]
+input("Now filling matrix")
+# taking values for matrix
+while True:
+    try:
+        matrix = [[int(input(f'Enter value for matrix[{row}][{col}]:')) for col in range(cols)] for row in range(rows)]
+        break
+    except ValueError:
+        print('Please enter only integer values')
+
+# displaying matrix
+print('matrix')
+for row in matrix:
+    print(row)
+
+# Transpose will be of order cols*rows
+# Transpose of matrix is swapping rows and columns
+transpose = [[matrix[row][col] for row in range(rows)] for col in range(cols)]
+
+print('Transpose')
 for row in transpose:
     print(row)             # displaying result
