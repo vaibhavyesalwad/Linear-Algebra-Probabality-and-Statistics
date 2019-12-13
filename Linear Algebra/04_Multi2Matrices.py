229dbb004d30440f25a9076bb5a61f4a2484140c
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-13 16:48:22 +0530

    Refactored: replaced hardcoded values by user input values

--- Linear Algebra/04_Multi2Matrices.py
+++ Linear Algebra/04_Multi2Matrices.py
@@ -1,15 +1,56 @@
 """Program to multiply matrices"""
+# taking order of two matrices from user
+while True:
+    try:
+        rows1 = int(input('Enter number of rows for matrix1:'))
+        cols1 = int(input('Enter number of columns for matrix1:'))
+        while True:
+            try:
+                rows2 = int(input('Enter number of rows for matrix2:'))
+                cols2 = int(input('Enter number of columns for matrix2:'))
+                assert cols1 == rows2             # condition for matrix multiplication
+                break
+            except ValueError:
+                print('Enter only integers')
+            except AssertionError:
+                print("Number of columns in matrix1 & number of rows in matrix2 don't match")
+        break
+    except ValueError:            # making sure inputs are integers
+        print('Enter only integers')
 
-X = [[12, 7, 3],
-     [4, 5, 6],               # For multiplication of matrices columns in matrix1 must be equal to rows in matrix2
-     [7, 8, 9]]               # Both matrices X and Y are of order 3*3
-Y = [[5, 8, 1],
-     [6, 7, 3],
-     [4, 5, 9]]
+input("Let's start filling matrix1")
+# taking values for matrix1
+while True:
+    try:
+        matrix1 = [[int(input(f'Enter value for matrix1[{row}][{col}]:')) for col in range(cols1)] for row in range(rows1)]
+        break
+    except ValueError:
+        print('Please enter only integer values')
 
-# resultant matrix will be of size 3*3 (for matrices of m*n and n*p resultant matrix will be m*p)
+input("Now filling matrix2")
+# taking values for matrix2
+while True:
+    try:
+        matrix2 = [[int(input(f'Enter value for matrix2[{row}][{col}]:')) for col in range(cols2)] for row in range(rows2)]
+        break
+    except ValueError:
+        print('Please enter only integer values')
+
+print('matrix1')
+# displaying first matrix
+for row in matrix1:
+    print(row)
+
+print('matrix2')
+# displaying second matrix
+for row in matrix2:
+    print(row)
+
+# resultant matrix will be of order rows1*cols2
 # multiplying rows of X with columns of Y
-multiplication = [[sum(X[i][k]*Y[k][j] for k in range(3)) for j in range(3)] for i in range(3)]
+multiplication = [[sum(matrix1[row][k]*matrix2[k][col] for k in range(cols1)) for col in range(cols2)] for row in range(rows1)]
+
+print('Multiplication of matrix1 & matrix2')
 for row in multiplication:
     print(row)                     # displaying result
 
