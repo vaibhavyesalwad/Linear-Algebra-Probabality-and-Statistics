bff41e825f48d1163f8d6d16e67182a75c9586f1
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-13 14:55:26 +0530

    Refactored: replaced hardcoded values by inputs from user

--- Linear Algebra/02_ScalarMultiMatrix.py
+++ Linear Algebra/02_ScalarMultiMatrix.py
@@ -1,9 +1,31 @@
 """Program to perform scalar multiplication of matrix and a number"""
-X = [[12, 7, 3],
-     [4, 5, 6],
-     [7, 8, 9]]
-Y = 9                               # scalar
+# taking order of matrix from user
+while True:
+    try:
+        rows = int(input('Enter number of rows for X matrix:'))
+        cols = int(input('Enter number of columns for X matrix:'))
+        break
+    except ValueError:
+        print('Enter only integers')
 
-scalar_multi = [[X[i][j] * Y for j in range(3)] for i in range(3)]      # multiplying each element of matrix with scalar
+# taking values for scalar and matrix X from user
+while True:
+    try:
+        scalar = int(input('Enter an integer as scalar:'))      # scalar is real number
+        input("Let's start filling values in matrix X")
+        # matrix is 2D list having numbers as it's elements here we are taking integers
+        X = [[int(input(f'Enter value for X[{row}][{col}]:'))for col in range(cols)] for row in range(rows)]
+        break
+    except ValueError:
+        print('Please enter only integer values')
+
+# displaying matrix
+print('Matrix X')
+for row in X:
+    print(row)
+
+print(f'Scalar multiplication of matrix X and {scalar} (scalar)')
+# multiplying each element of matrix X by with the scalar
+scalar_multi = [[X[row][col] * scalar for col in range(cols)] for row in range(rows)]
 for row in scalar_multi:
     print(row)                # displaying result
