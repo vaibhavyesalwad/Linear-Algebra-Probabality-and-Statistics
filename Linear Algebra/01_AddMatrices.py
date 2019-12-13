4c505cfaa11e5ca96b76a098e088addc150e1e98
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-13 16:56:14 +0530

    Refactored: Used proper naming conventions

--- Linear Algebra/01_AddMatrices.py
+++ Linear Algebra/01_AddMatrices.py
@@ -2,57 +2,59 @@
 # taking order of first matrix
 while True:
     try:
-        rows1 = int(input('Enter number of rows for X matrix:'))
-        cols1 = int(input('Enter number of columns for X matrix:'))
+        rows1 = int(input('Enter number of rows for matrix1:'))
+        cols1 = int(input('Enter number of columns for matrix2:'))
         break
     except ValueError:                # making sure input values are integers
         print('Enter only integers')
 
-print(f'Order of matrix X is {rows1}*{cols1}')
+print(f'Order of matrix1 is {rows1}*{cols1}')
 
 # taking order of second matrix & checking if it matches with first matrix
 while True:
     try:
-        rows2 = int(input('Enter number of rows for Y matrix:'))
-        cols2 = int(input('Enter number of columns for Y matrix:'))
+        rows2 = int(input('Enter number of rows for matrix2:'))
+        cols2 = int(input('Enter number of columns for matrix2:'))
         print(f'Order of matrix Y is {rows2}*{cols2}')
         assert rows1 == rows2 and cols1 == cols2
         break
     except ValueError:
         print('Enter only integers')
     except AssertionError:
-        print('Number of rows and columns for both matrices X & Y must match')
+        print('Number of rows and columns for both matrices- matrix1 & matrix2 must match')
 
-print(f'Order of both matrices X & Y is same i.e {rows2}*{cols2}, now we can add them')
-input("Let's start filling matrix X")
+print(f'Order of both matrices- matrix1 & matrix2 is same i.e {rows2}*{cols2}, now we can add them')
+input("Let's start filling matrix1")
 
 # taking values for first matrix from user
 while True:
     try:
-        X = [[int(input(f'Enter value for X[{row}][{col}]:'))for col in range(cols1)] for row in range(rows1)]
+        matrix1 = [[int(input(f'Enter value for matrix1[{row}][{col}]:'))for col in range(cols1)] for row in range(rows1)]
         break
     except ValueError:
         print('Please enter only integer values')
 
-input("Let's start filling matrix Y")
+input("Let's start filling matrix2")
 
 # taking values for second matrix from user
 while True:
     try:
-        Y = [[int(input(f'Enter value for Y[{row}][{col}]:')) for col in range(cols2)] for row in range(rows2)]
+        matrix2 = [[int(input(f'Enter value for matrix2[{row}][{col}]:')) for col in range(cols2)] for row in range(rows2)]
         break
     except ValueError:
         print('Please enter only integer values')
 
-print('Matrix X')
-for row in X:
+# displaying matrices
+print('matrix1')
+for row in matrix1:
     print(row)
-print('Matrix Y')
-for row in Y:
+
+print('matrix2')
+for row in matrix2:
     print(row)
 
 # adding corresponding elements of matrices
-addition = [[X[row][col] + Y[row][col] for col in range(cols1)]for row in range(rows1)]
-print('Addition of matrices X & Y')
+addition = [[matrix1[row][col] + matrix2[row][col] for col in range(cols1)]for row in range(rows1)]
+print('Addition of matrix1 & matrix2')
 for row in addition:
     print(row)             # displaying result
