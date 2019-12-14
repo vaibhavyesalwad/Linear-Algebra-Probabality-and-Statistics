5ac41b2236e4c5fe14fb979aba3342462d6365f1
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-14 11:35:24 +0530

    Added: allowed user inputs for matrix and built fn for determinant of matrix

--- Linear Algebra/05_InverseOfMatrix.py
+++ Linear Algebra/05_InverseOfMatrix.py
@@ -1,33 +1,63 @@
-"""Program to find inverse matrix of given 3*3 matrix """
+"""Program to find inverse matrix of given matrix """
+from copy import deepcopy                      # it is crucial for this program
+# taking order of matrix from user
+while True:
+    try:
+        # only square matrix can have inverse
+        print('Only square matrix are invertible')
+        rows = cols = int(input('Enter number of rows/columns for matrix:'))
+        break
+    except ValueError:  # making sure input integer values
+        print('Enter only integers')
+
+input("Now filling matrix")
+# taking values for matrix
+while True:
+    try:
+        matrix = [[int(input(f'Enter value for matrix[{row}][{col}]:')) for col in range(cols)] for row in range(rows)]
+        break
+    except ValueError:
+        print('Please enter only integer values')
 
 
-def minor(row, col):
-    """Function returns determinant of remaining 2*2 matrix after ignoring given row & column in 3*3 matrix"""
-    rows = [k for k in range(3)]
-    rows.remove(row)                    # ignoring given row
-    cols = [k for k in range(3)]
-    cols.remove(col)                    # ignoring given column
-    a, b = rows
-    c, d = cols
-    return X[a][c]*X[b][d] - X[b][c]*X[a][d]         # determinant of 2*2 matrix
+def minor(mat, r, c):
+    """Function returns matrix ignoring given row & column of given matrix"""
+    mat.pop(r)                 # ignoring row
+    for row in mat:
+        row.pop(c)             # ignoring column
+    return mat
 
 
-X = [[12, 7, 3],
-     [4, 5, 6],                      # given matrix
-     [7, 8, 9]]
+def determinant(mat):
+    """Function recursively finds determinant of given matrix"""
+    # when matrix of order is 1
+    if len(mat) == 1:
+        return mat[0][0]
+    else:
+        # finding determinant using first row
+        # we have used deepcopy here because matrix has nested elements thus it allows matrix manipulation
+        return sum(mat[0][col] * determinant(minor(deepcopy(mat), 0, col)) if col % 2 == 0
+                   else -mat[0][col] * determinant(minor(deepcopy(mat), 0, col)) for col in range(len(mat)))
 
-# determinant decides if inverse possible or not so calculating it first
-determinant = X[0][0]*minor(0, 0) - X[0][1]*minor(0, 1) + X[0][2]*minor(0, 2)
-if determinant:
+
+print('matrix')
+# displaying matrix
+for row in matrix:
+    print(row)
+
+print(f'Determinant of matrix: {determinant(matrix)}')
+'''
+if determinant(matrix):
     # finding minors matrix 3*3 using 2*2 matrices after ignoring current row & column
-    minors_matrix = [[minor(row, col) for col in range(3)] for row in range(3)]
+    minors_matrix = [[determinant(minor(deepcopy(matrix), row, col)) for col in range(cols)] for row in range(rows)]
     # cofactors matrix is like chess box alternate positions are negated
     cofactors_matrix = [[-minors_matrix[row][col] if (row+col) % 2 else minors_matrix[row][col] for col in range(3)] for row in range(3)]
-    # adjugate is transpose of cofactors matrix
-    adjugate = [[cofactors_matrix[col][row] for col in range(3)] for row in range(3)]
+    # adjoint is transpose of cofactors matrix
+    adjoint = [[cofactors_matrix[row][col] for row in range(rows)] for col in range(cols)]
     # inverse matrix is result of division of adjugate matrix by determinant of given matrix
     inverse = [[adjugate[row][col]/determinant for col in range(3)] for row in range(3)]
     for row in inverse:
         print(row)                # displaying result
 else:
     print('Matrix has no inverse as determinant is 0')
+'''
