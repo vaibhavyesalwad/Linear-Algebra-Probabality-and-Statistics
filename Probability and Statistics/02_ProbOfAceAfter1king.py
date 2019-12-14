cf2b2e349e1f77745b97d12770f62746f52a49ef
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-14 12:53:16 +0530

    Refactored: used proper naming conventions

--- Probability and Statistics/02_ProbOfAceAfter1king.py
+++ Probability and Statistics/02_ProbOfAceAfter1king.py
@@ -1,10 +1,10 @@
 """Program to find the probability of drawing an ace after drawing a king on the first draw"""
-from fractions import Fraction as F
+from fractions import Fraction
 cards = 52        # total cards in total pack
 aces = 4           # total ace cards
 # after removing king in first draw
 cards -= 1            # total cards will be reduced but aces will be the same
 # showing probability in fraction
-print(f'Probability of an ace from pack of cards after drawing king in 1st draw: {F(aces, cards)}')
+print(f'Probability of an ace from pack of cards after drawing king in 1st draw: {Fraction(aces, cards)}')
 # showing probability in percentage
 # print(f'Probability of an ace from pack of cards after drawing king in 1st draw: {round(aces/cards,4)*100} %')
