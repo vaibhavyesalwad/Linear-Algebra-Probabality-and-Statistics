d4149a61be1048a03ef7fc54c1245d4bfd4dc063
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-14 13:08:19 +0530

    Refactored: used proper naming conventions

--- Probability and Statistics/05_ProbRainTraffic.py
+++ Probability and Statistics/05_ProbRainTraffic.py
@@ -8,20 +8,20 @@ You pick a random day.
 2. Probability that I am late?
 3 .Given that I arrived late at work, what is the probability that it rained that day
 """
-from fractions import Fraction as F
-p_rain = F(1, 3)           # probability of day is rainy
-p_rain_traffic = F(1, 2)       # probability of heavy traffic after rain
-p_noRain_traffic = F(1, 4)     # probability of heavy traffic after no rain
+from fractions import Fraction
+p_rain = Fraction(1, 3)           # probability of day is rainy
+p_rain_traffic = Fraction(1, 2)       # probability of heavy traffic after rain
+p_noRain_traffic = Fraction(1, 4)     # probability of heavy traffic after no rain
 # probability of getting late after rain followed by heavy traffic
-p_rain_traffic_late = F(1, 2)
+p_rain_traffic_late = Fraction(1, 2)
 # if no rain followed by no heavy traffic
-p_noRain_noTraffic_late = F(1, 8)
+p_noRain_noTraffic_late = Fraction(1, 8)
 # other remaining conditions not rainy followed by heavy traffic and rain followed by no heavy traffic
-p_rain_noTraffic_late = p_noRain_traffic_late = F(1, 4)
+p_rain_noTraffic_late = p_noRain_traffic_late = Fraction(1, 4)
 
-# probability of not late after all conditions is found by multiplying all prior probabilities to be encountered
-result = (1-p_rain) * p_noRain_traffic *(1-p_noRain_traffic_late)
-print(f'P (NOT getting late when no rain followed by heavy traffic): {result}')
+# probability of not late after earlier conditions is found by multiplying all prior probabilities to be encountered
+result = (1-p_rain) * p_noRain_traffic * (1-p_noRain_traffic_late)
+print(f'P (NOT getting late when NO rain followed by heavy traffic): {result}')
 # adding all conditions for getting late i.e four conditions for being late
 cond1 = p_rain*p_rain_traffic*p_rain_traffic_late
 cond2 = p_rain*(1-p_rain_traffic)*p_rain_noTraffic_late
@@ -30,6 +30,6 @@ cond4 = (1-p_rain)*(1-p_noRain_traffic)*p_noRain_noTraffic_late
 all_late = cond1 + cond2 + cond3 + cond4
 print(f'P (getting late in all situations): {all_late}')
 # probability of it rained given I'm late
-print(f"P (it rained given I'm late): {(cond1+ cond2)/all_late}")
+print(f"P (it rained / I'm late): {(cond1+ cond2)/all_late}")
 
 
