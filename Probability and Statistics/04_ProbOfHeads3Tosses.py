fa7425a56628c93b4f05e79a2144c7bad64ddeda
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-14 12:59:54 +0530

    Refactored: used proper naming conventions

--- Probability and Statistics/04_ProbOfHeads3Tosses.py
+++ Probability and Statistics/04_ProbOfHeads3Tosses.py
@@ -5,7 +5,7 @@
 4. Probability of at least two heads"""
 
 from itertools import product
-from fractions import Fraction as F
+from fractions import Fraction
 sides = {'H', 'T'}               # set sides of coin
 # finding total outcomes than can occur i.e.  sample space
 sample_space = list(product(sides, sides, sides))   # we are using cartesian product as coin tossed 3 times
@@ -14,13 +14,13 @@ print(f'Sample space: {sample_space}')   # sample space for tossing 3 coins
 
 # using generator expression to count total required conditions
 favourable = sum(result.count('H') == 3 for result in sample_space)         # for exactly 3 heads
-print(f'P (exactly 3 heads): {F(favourable, len(sample_space))}')     # Probability in fraction
+print(f'P (exactly 3 heads): {Fraction(favourable, len(sample_space))}')     # Probability in fraction
 
 favourable = sum(result.count('H') == 1 for result in sample_space)         # for exactly 1 head
-print(f'P (exactly 1 head): {F(favourable, len(sample_space))}')  # Probability in fraction
+print(f'P (exactly 1 head): {Fraction(favourable, len(sample_space))}')  # Probability in fraction
 
 favourable = sum(result.count('H') >= 1 for result in sample_space)           # for at least 1 head
-print(f'P (at least 1 head): {F(favourable, len(sample_space))}')  # Probability in fraction
+print(f'P (at least 1 head): {Fraction(favourable, len(sample_space))}')  # Probability in fraction
 
 favourable = sum(result.count('H') >= 2 for result in sample_space)            # for at least 2 heads
-print(f'P (at least 2 heads): {F(favourable, len(sample_space))}')  # Probability in fraction
+print(f'P (at least 2 heads): {Fraction(favourable, len(sample_space))}')  # Probability in fraction
